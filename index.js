42f21961b52793f4fd0bda2e210331550f45b12c
Cameron Ford <c.r.ford@outlook.com>
2022-04-13 21:51:35 -0600

    1st commit pt2 yay

---
+++ index.js
@@ -0,0 +1,33 @@
+const colors = ["red", "blue", "green"]
+
+document.getElementById("add").addEventListener("onclick",subItem) 
+function(e){
+     const subItem = createSubItem(e)
+    document.getElementById("list").appendChild(subItem)
+}
+
+function createDropDown(){
+    const dropDown = document.body.createElement("select")
+    for (let i = 0; i < i; i++){
+        const option = createElement("option") 
+        // option.innerHTML = colors[i]
+        // option.value = colors[i]
+        document.body.append("option")
+    }1
+    dropDown.addEventListener("onchange", function(e){
+        e.target.parent.backgroundColor = e.target.value
+    })
+    return dropDown
+}
+
+function createSubItem(e){
+    const subItem = document.createElement("div")
+    var subItemValue = document.getElementById("input")
+    subItem.textContent = subItemValue
+    const dropDown = createDropDown()
+    subItem.appendChild(dropDown)
+    subItem.setAttribute("class", "subItem")
+    return subItem
+}
+
+
